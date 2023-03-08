584b3bd0ccdaea750799b08e194133d3ee0df321
Jack <96661942+jpshrader@users.noreply.github.com>
2023-03-08 08:23:06 +0000

    tidy

--- main.go
+++ main.go
@@ -12,17 +12,14 @@ import (
 const BASE_URL = "/api"
 
 func main() {
-    log.Println("starting...")
-    http.HandleFunc(BASE_URL, root)
+	log.Println("starting...")
+	http.HandleFunc(BASE_URL, root)
 
-    http.HandleFunc(fmt.Sprintf("%s/plants", BASE_URL), plants.GetPlants)
+	http.HandleFunc(fmt.Sprintf("%s/plants", BASE_URL), plants.GetPlants)
 
-
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-
-    response.JsonEncode(w, http.StatusOK, "Welcome to the Scott Arboretum Api!")
+	response.JsonEncode(w, http.StatusOK, "Welcome to the Scott Arboretum Api!")
 }
